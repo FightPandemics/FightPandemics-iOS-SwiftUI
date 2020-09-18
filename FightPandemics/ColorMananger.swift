a6bea1fe6255528667dd62097701b53bdd3913fe
Luko <luko.web@gmail.com>
2020-09-18 11:55:37 -0300

    Import Assets Colour Pallete

---
+++ FightPandemics/ColorMananger.swift
@@ -0,0 +1,29 @@
+//
+//  ColorMananger.swift
+//  FightPandemics
+//
+//  Created by Luciano Schillagi on 9/18/20.
+//  Copyright © 2020 Scott. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorManager {
+    static let black = Color("black")
+    static let black30 = Color("black30")
+    static let blackThree = Color("blackThree")
+    static let blackTwo = Color("blackTwo")
+    static let brownGrey = Color("brownGrey")
+    static let brownGreyTwo = Color("brownGreyTwo")
+    static let colorCorrectOpen = Color("colorCorrectOpen")
+    static let colorErrorExpired = Color("colorErrorExpired")
+    static let colorSecondaryBrandColor = Color("colorSecondaryBrandColor")
+    static let colorSecondaryOffWhite = Color("colorSecondaryOffWhite")
+    static let paleGrey = Color("paleGrey")
+    static let paleGreyTwo = Color("paleGreyTwo")
+    static let purpleishBlue = Color("purpleishBlue")
+    static let veryLightPink = Color("veryLightPink")
+    static let veryLightPinkTwo = Color("veryLightPinkTwo")
+    static let white = Color("white")
+}
+    
