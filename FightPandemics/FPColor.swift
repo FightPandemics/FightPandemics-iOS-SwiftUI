c77ed661bd3d750b6e92717e8bf9e99f42a86b04
Luko <luko.web@gmail.com>
2020-10-01 11:39:48 -0300

    Imports Colours Pallete

---
+++ FightPandemics/FPColor.swift
@@ -0,0 +1,32 @@
+//
+//  FPColor.swift
+//  FightPandemics
+//
+//  Created by Luciano Schillagi on 9/18/20.
+//  Copyright © 2020 Scott. All rights reserved.
+//
+
+import SwiftUI
+
+// Abstract: Fight Pandemics Colors Catalog
+struct FPColor {
+    static let black = Color("black")
+    static let black30 = Color("black30")
+    static let blackThree = Color("blackThree")
+    static let blackTwo = Color("blackTwo")
+    static let brownGrey = Color("brownGrey")
+    static let brownGreyTwo = Color("brownGreyTwo")
+    static let colorCorrectOpen = Color("colorCorrectOpen")
+    static let colorErrorExpired = Color("colorErrorExpired")
+    static let colorSecondaryBrandColor = Color("colorSecondaryBrandColor")
+    static let colorSecondaryOffWhite = Color("colorSecondaryOffWhite")
+    static let paleGrey = Color("paleGrey")
+    static let paleGreyTwo = Color("paleGreyTwo")
+    static let purpleishBlue = Color("purpleishBlue")
+    static let veryLightPink = Color("veryLightPink")
+    static let veryLightPinkTwo = Color("veryLightPinkTwo")
+    static let white = Color("white")
+}
+
+// https://app.zeplin.io/project/5ecaeec0e0303147915effa8/styleguide/colors
+    
